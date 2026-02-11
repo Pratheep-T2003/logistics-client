866e7fcce6544899d7b332c42035c4e024072cc8
Pratheep | MCA <129410816+Pratheep-T2003@users.noreply.github.com>
2026-02-11 12:59:45 +0530

    Delete src directory

--- src/api/shipmentService.js
+++
@@ -1,6 +0,0 @@
-import api from './axios';
-
-export const getShipments = () => api.get('/shipments');
-export const createShipment = (data) => api.post('/shipments', data);
-export const updateShipmentStatus = (id, status, note, assignedDriver) => api.patch(`/shipments/${id}/status`, { status, note, assignedDriver });
-export const deleteShipment = (id) => api.delete(`/shipments/${id}`);
